e889381956902ef1060e568b0c11d6851d3e83bb
Patrick Schnetger <patrick.schnetger@gmail.com>
2016-04-30 00:05:44 +0200

    fixes the attachment spec

--- spec/easybill/api/attachments_spec.rb
+++ spec/easybill/api/attachments_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe Easybill::Api::Attachments do
+  describe ".resource_path" do
+    it "has the right resource_path set" do
+      expect(Easybill::Api::Attachments.send(:resource_path)).to eq("/rest/v1/attachments")
+    end
+  end
+
   describe ".content" do
     let(:id) { 1234 }
 
     it "returns the content of an attachment" do
-      expect(Easybill::Api::Attachments).to receive(:custom_request).with(
+      expect(Easybill::Api::Attachments).to receive(:custom).with(
         method: :get,
         path: "/rest/v1/attachments/#{id}/content"
       )
