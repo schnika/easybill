e56abe8f1cdde0313545f4bde9eb35babfeba71d
Patrick Schnetger <patrick.schnetger@gmail.com>
2016-05-01 21:30:22 +0200

    full api support

--- lib/easybill/api/client.rb
+++ lib/easybill/api/client.rb
@@ -5,16 +5,56 @@ module Easybill
         Easybill::Api::Base.authenticate! api_key
       end
 
-      def customers
-        Easybill::Api::Customers
-      end
-
       def attachments
         Easybill::Api::Attachments
       end
 
       def contacts
         Easybill::Api::Contacts
+      end
+
+      def customer_groups
+        Easybill::Api::CustomerGroups
+      end
+
+      def customers
+        Easybill::Api::Customers
+      end
+
+      def document_payments
+        Easybill::Api::DocumentPayments
+      end
+
+      def documents
+        Easybill::Api::Documents
+      end
+
+      def position_groups
+        Easybill::Api::PositionGroups
+      end
+
+      def positions
+        Easybill::Api::Positions
+      end
+
+      def post_boxes
+        Easybill::Api::PostBoxes
+      end
+
+      def projects
+        Easybill::Api::Projects
+      end
+
+      def tasks
+        Easybill::Api::Tasks
+      end
+
+      def text_templates
+        Easybill::Api::TextTemplates
+      end
+
+      def time_trackings
+        Easybill::Api::TimeTrackings
       end
     end
   end
--- spec/easybill/api/client_spec.rb
+++ spec/easybill/api/client_spec.rb
@@ -11,9 +11,81 @@ describe Easybill::Api::Client do
     end
   end
 
+  describe "#attachments" do
+    it "returns a Easybill::Api::Attachments instance" do
+      expect(subject.attachments).to be(Easybill::Api::Attachments)
+    end
+  end
+
+  describe "#contacts" do
+    it "returns a Easybill::Api::Contacts instance" do
+      expect(subject.contacts).to be(Easybill::Api::Contacts)
+    end
+  end
+
+  describe "#customer_groups" do
+    it "returns a Easybill::Api::CustomerGroups instance" do
+      expect(subject.customer_groups).to be(Easybill::Api::CustomerGroups)
+    end
+  end
+
   describe "#customers" do
-    it "returns a Easybill::Api::Customer instance" do
+    it "returns a Easybill::Api::Customers instance" do
       expect(subject.customers).to be(Easybill::Api::Customers)
+    end
+  end
+
+  describe "#document_payments" do
+    it "returns a Easybill::Api::DocumentPayments instance" do
+      expect(subject.document_payments).to be(Easybill::Api::DocumentPayments)
+    end
+  end
+
+  describe "#documents" do
+    it "returns a Easybill::Api::Documents instance" do
+      expect(subject.documents).to be(Easybill::Api::Documents)
+    end
+  end
+
+  describe "#position_groups" do
+    it "returns a Easybill::Api::PositionGroups instance" do
+      expect(subject.position_groups).to be(Easybill::Api::PositionGroups)
+    end
+  end
+
+  describe "#positions" do
+    it "returns a Easybill::Api::Positions instance" do
+      expect(subject.positions).to be(Easybill::Api::Positions)
+    end
+  end
+
+  describe "#post_boxes" do
+    it "returns a Easybill::Api::PostBoxes instance" do
+      expect(subject.post_boxes).to be(Easybill::Api::PostBoxes)
+    end
+  end
+
+  describe "#projects" do
+    it "returns a Easybill::Api::Pojects instance" do
+      expect(subject.projects).to be(Easybill::Api::Projects)
+    end
+  end
+
+  describe "#tasks" do
+    it "returns a Easybill::Api::Tasks instance" do
+      expect(subject.tasks).to be(Easybill::Api::Tasks)
+    end
+  end
+
+  describe "#text_templates" do
+    it "returns a Easybill::Api::TextTemplates instance" do
+      expect(subject.text_templates).to be(Easybill::Api::TextTemplates)
+    end
+  end
+
+  describe "#time_trackings" do
+    it "returns a Easybill::Api::TimeTrackings instance" do
+      expect(subject.time_trackings).to be(Easybill::Api::TimeTrackings)
     end
   end
 end
