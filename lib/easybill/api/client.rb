06235862f29c4f60895bbbab385d3b72ea808c46
Patrick Schnetger <patrick.schnetger@gmail.com>
2016-05-01 22:28:19 +0200

    add rdoc to the client

--- lib/easybill/api/client.rb
+++ lib/easybill/api/client.rb
@@ -1,57 +1,108 @@
 module Easybill
   module Api
+
+    ##
+    # This class represents the client object.
+    # Use this class to initialize you api client
+
     class Client
+
+      ##
+      # Create a new client instance with an +api_key+
+      #
+      # Create or use your generated easybill +api_key+ here
+      # @api = Easybill::Api::Client.new("api_key")
+
       def initialize(api_key)
         Easybill::Api::Base.authenticate api_key
       end
+
+      ##
+      # Returns a Easybill::Api::Attachments class
 
       def attachments
         Easybill::Api::Attachments
       end
 
+      ##
+      # Returns a Easybill::Api::Contacts class
+
       def contacts
         Easybill::Api::Contacts
       end
+
+      ##
+      # Returns a Easybill::Api::CustomerGroups class
 
       def customer_groups
         Easybill::Api::CustomerGroups
       end
 
+      ##
+      # Returns a Easybill::Api::Customers class
+
       def customers
         Easybill::Api::Customers
       end
+
+      ##
+      # Returns a Easybill::Api::DocumentPayments class
 
       def document_payments
         Easybill::Api::DocumentPayments
       end
 
+      ##
+      # Returns a Easybill::Api::Documents class
+
       def documents
         Easybill::Api::Documents
       end
+
+      ##
+      # Returns a Easybill::Api::PositionGroups class
 
       def position_groups
         Easybill::Api::PositionGroups
       end
 
+      ##
+      # Returns a Easybill::Api::Positions class
+
       def positions
         Easybill::Api::Positions
       end
+
+      ##
+      # Returns a Easybill::Api::PostBoxes class
 
       def post_boxes
         Easybill::Api::PostBoxes
       end
 
+      ##
+      # Returns a Easybill::Api::Projects class
+
       def projects
         Easybill::Api::Projects
       end
+
+      ##
+      # Returns a Easybill::Api::Tasks class
 
       def tasks
         Easybill::Api::Tasks
       end
 
+      ##
+      # Returns a Easybill::Api::TextTemplates class
+
       def text_templates
         Easybill::Api::TextTemplates
       end
+
+      ##
+      # Returns a Easybill::Api::TimeTrackings class
 
       def time_trackings
         Easybill::Api::TimeTrackings
