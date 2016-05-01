3a4e26d7296108b8991b5d2c9df1965d346364db
Patrick Schnetger <patrick.schnetger@gmail.com>
2016-05-01 22:28:09 +0200

    add rdoc for the base class

--- lib/easybill/api/base.rb
+++ lib/easybill/api/base.rb
@@ -2,6 +2,18 @@ require 'httparty'
 
 module Easybill
   module Api
+
+    ##
+    # The base class for all Easybill entities.
+    # Set the authorization header before using it via `.authenticate`
+    #
+    # Sets the default headers:
+    # "User-Agent"   => "Ruby.Easybill.Api",
+    # "Accept"       => "application/json",
+    # "Content-Type" => "application/json"
+    #
+    # Provides the basic interface. Such as list, find, create, update, destroy
+
     class Base
       include HTTParty
 
@@ -16,25 +28,61 @@ module Easybill
       format :json
 
       class << self
+
+        ##
+        # Set the authorization header by providing your easybill +api_key+
+
         def authenticate(api_key)
           headers["authorization"] = "Bearer #{api_key}"
         end
+
+        ##
+        # Fetches all resources. You can set custom +query+ parameters.
 
         def list(query: {})
           get resource_path, query: query
         end
 
+        ##
+        # Fetches a resource by its +id+. You can set custom +query+ parameters.
+        # api.find(id, query: {group: 1})
+
         def find(id, query: {})
           get "#{resource_path}/#{id}", query: query
         end
+
+        ##
+        # Creates a resource. Provide the +data+ as a hash.
+        # e.g.
+        # data = {
+        #   first_name: "Lars",
+        #   last_name: "Bar",
+        #   ...
+        # }
+        # api.create(data)
 
         def create(data)
           post resource_path, body: data.to_json
         end
 
+        ##
+        # Updates a resource. Provide the +id+ and +data+.
+        # e.g.
+        # id = 123456
+        # data = {
+        #   first_name: "Lars",
+        #   last_name: "Bar",
+        #   ...
+        # }
+        # api.update(id, data)
+
         def update(id, data)
           put "#{resource_path}/#{id}", body: data.to_json
         end
+
+        ##
+        # Destroys a resource. Provide the +id+.
+        # api.destroy(id)
 
         def destroy(id)
           delete "#{resource_path}/#{id}"
@@ -62,6 +110,7 @@ module Easybill
           "#{base_path}/#{dasherize(self.name.split("::").last)}"
         end
 
+        # TODO-schnika: move this out of here Oo
         def dasherize(camel_cased_word)
           camel_cased_word.to_s.gsub(/::/, '/').
           gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
